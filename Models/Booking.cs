6f2079baeec78c9e3d5c3a75c90d1583c3fd8d53
kamindu.g <kamindu.g@informaticsint.com>
2023-10-09 23:48:31 +0530

    ticket booking controller

---
+++ Models/Booking.cs
@@ -0,0 +1,25 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace TicketReservation.Models
+{
+    public class BookingDetails
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string CusName { get; set; } = null!;
+
+        public string Bookdate { get; set; } = null!;
+
+        public string From { get; set; } = null!;
+
+        public string To { get; set; } = null!;
+
+        public string Traintime { get; set; } = null!;
+
+        public string IsActive { get; set; } = null!;
+    }
+}
